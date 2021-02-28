0e0410fee7f8354c2c716f7fc0bd7d853e430a11
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-02-28 16:04:36 -0500

    CSS added

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const { formats } = require("dayjs/locale/*");
+// const { formats } = require("dayjs/locale/*");
+const dice = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
 
-const dice = [
-    1, 2, 3, 4, 5, 6
-]
-
-let randomDiceRoll = dice[Math.floor(Math.random() * dice.length)];
-
-const numOfDiceInput = document.querySelector("#number-of-dice-input"); //grabs element of the number of dice to roll
+const input = document.querySelector("#number-of-dice-input"); //grabs element of the number of dice to roll
 let pDiceRolled = document.querySelector("#dice-para"); // grabs p element that stores values of each dice rolled in one roll event
 let pDiceRollSum = document.querySelector("#sum-para"); // grabs p element that stores sum of above values
-let ul = document.querySelector("#roll-history"); //
+const ul = document.querySelector("#roll-history"); //
 const button = document.querySelector("#generate-dice-roll-btn");
-
-let sum = 0
-let numOfDice = 0
+const original = pDiceRolled.textContent;
 
 button.addEventListener("click", (e) => {
     e.preventDefault()
-    pDiceRolled.textContent = "";
-    for (face of dice) {
-        sum += (numOfDiceInput.value * randomDiceRoll)
+    let sum = 0
+    let diceRoll = []
+    pDiceRollSum.textContent = "Sum ="
+    pDiceRolled.textContent = original;
+    for (let i = 0; i < input.valueAsNumber; i++) {
+        const randomIndex = Math.floor(Math.random() * dice.length)
+        const randomDiceRoll = dice[randomIndex];
+        diceRoll.push(randomDiceRoll)
+        sum += randomIndex + 1
     }
+    pDiceRolled.textContent = diceRoll.join("")
     pDiceRollSum.textContent += sum
+    
+    const li = document.createElement("li")
+    li.textContent = `${diceRoll.join("")} = ${sum}`
+    ul.appendChild(li);
 })
-const li = document.createElement("li")
-li.innerText = ``
-ul.appendChild(li);
